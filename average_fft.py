693b9615ac3b93dc9abd062cef2090c0d3ff30c8
abx393 <abandari99@gmail.com>
2021-04-21 20:07:52 -0700

    different keyboard types

--- average_fft.py
+++ average_fft.py
@@ -1,5 +1,5 @@
 """
-Produces Fourier Transform plots.
+Computes the average Fourier Transform for each key.
 """
 
 import os
@@ -12,6 +12,9 @@ from json import JSONDecoder
 DIR = "raw_data"
 KEYBOARD_TYPE = "mechanical"
 offset = 20000
+
+avg_fft = {}
+key_count = {}
 
 for f in os.listdir(os.path.join(DIR, KEYBOARD_TYPE)):
     print(f)
@@ -30,32 +33,66 @@ for f in os.listdir(os.path.join(DIR, KEYBOARD_TYPE)):
 
             # timestamp is milliseconds since start of audio
             timestamp = int(timestamp)
-            print("timestamp ", timestamp)
 
             # Get the range of samples associated with the current key press
             sample_start = timestamp * sample_rate // 1000 - offset
             sample_end = timestamp * sample_rate // 1000 + offset
-            print("sample_start ", sample_start)
-            print("sample_end ", sample_end)
 
             # number of time samples or number of frequency bins
             n = sample_end - sample_start
 
             freq = np.fft.fftfreq(n, 1 / sample_rate)
-            magnitude = np.fft.fft(samples[sample_start : sample_end])
+            ampltitude = np.fft.fft(samples[sample_start : sample_end])
 
             # only look at positive frequencies
             freq = freq[: n//2]
-            magnitude = magnitude[: n//2]
+            ampltitude = ampltitude[: n//2]
 
-            plt.plot(freq, magnitude)
+            # Store fft's by key
+            if label not in avg_fft:
+                avg_fft[label] = ampltitude
+                key_count[label] = 1
+            else:
+                print("before avg_fft[label] ", avg_fft[label])
+                avg_fft[label] += ampltitude
+                print("after avg_fft[label] ", avg_fft[label])
+                key_count[label] += 1
+
+            """
+            plt.plot(freq, ampltitude)
             # plt.axis([0, 10000, 0, 300000])
             plt.xlim([0, 5000])
             plt.ylim(bottom=0)
-            plt.title("key = {}, time = {} ms, keyboard = {}".format(label, timestamp, KEYBOARD_TYPE))
+            plt.title("key = {}, time = {} ms".format(label, timestamp))
             plt.xlabel("Frequency (Hz)")
             plt.ylabel("Amplitude")
             plt.show()
+            """
 
         labels_file.close()
+
+print("key_count " + str(key_count))
+# print("avg_fft " + average_fft)
+
+fs = 48000
+for key in avg_fft:
+    # print("key ", key)
+    amplitude = avg_fft[key]
+    # print("amplitude ", amplitude)
+
+    # Normalize by total number of data points for this key
+    # ampltitude /= key_count[key]
+
+    freq = np.fft.fftfreq(offset * 2, 1 / fs)[: offset]
+
+    # Plot the average fft for this key
+    plt.clf()
+    plt.plot(freq, ampltitude)
+    plt.xlim([0, 5000])
+    plt.ylim(bottom=0)
+    plt.title("key = {}".format(key))
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Amplitude")
+    plt.show()
+
 
--- spectrogram.py
+++ spectrogram.py
@@ -1,3 +1,7 @@
+"""
+Produces spectrogram plots.
+"""
+
 import os
 
 from scipy.io import wavfile
@@ -6,24 +10,26 @@ from matplotlib import pyplot as plt
 from json import JSONDecoder
 
 DIR = "raw_data"
+KEYBOARD_TYPE = "mechanical"
 
-for f in os.listdir(DIR):
+for f in os.listdir(os.path.join(DIR, KEYBOARD_TYPE)):
     print(f)
     (basename, extension) = f.split(".")
 
     # If it's a wav file, process it
     if extension == "wav":
-        sample_rate, samples = wavfile.read(os.path.join(DIR, f))
+        sample_rate, samples = wavfile.read(os.path.join(DIR, KEYBOARD_TYPE, f))
 
         # Get corresponding ground truth JSON file
-        labels_file = open(os.path.join(DIR, basename + ".json"))
+        labels_file = open(os.path.join(DIR, KEYBOARD_TYPE, basename + ".json"))
         labels = JSONDecoder().decode(labels_file.read())
 
         # Compute spectrogram
-        freqs, times, spec = spectrogram(samples, sample_rate, nperseg=20000, noverlap=5000)
+        freqs, times, spec = spectrogram(samples, sample_rate, nperseg=10000, noverlap=5000)
 
         # Plot spectrogram using color map
-        plt.pcolormesh(times, freqs, spec, cmap="inferno", vmin=0, vmax=1000)
+        plt.pcolormesh(times, freqs, spec, vmin=0, vmax=1000)
+        plt.title("Keyboard = " + KEYBOARD_TYPE)
         plt.xlabel("Time (s)")
         plt.ylabel("Frequency (Hz)")
         plt.ylim([0, 5000])

6f1f0a11d416ac59e5c8f9a05f07471828bb3d4d
abx393 <abandari99@gmail.com>
2021-05-05 21:49:14 -0700

    pyaudio data collection

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,3 +1,6 @@
+import os
+import time
+import keyboard
 import pyaudio
 import wave
 
@@ -5,16 +8,27 @@ CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-RECORD_SECONDS = 10
-WAVE_OUTPUT_FILENAME = "../output_membrane.wav"
+RECORD_SECONDS = 3
+DIR_OUT = "native_raw_data"
+
+timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+ground_truth = "key,time\n"
+
+def on_press(event):
+    global ground_truth
+    ground_truth = ground_truth + "{},{}\n".format(event.name, event.time - time_start)
+
 
 p = pyaudio.PyAudio()
 
+time_start = time.time()
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
+
+keyboard.on_press(on_press)
 
 print("Recording started...")
 frames = []
@@ -26,11 +40,16 @@ print("Recording stopped.")
 
 stream.stop_stream()
 stream.close()
+print("ground_truth ", ground_truth)
 p.terminate()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf = wave.open(os.path.join(DIR_OUT, timestamp + ".wav"), 'wb')
 wf.setnchannels(CHANNELS)
 wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
 wf.writeframes(b''.join(frames))
 wf.close()
+
+tf = open(os.path.join(DIR_OUT, timestamp + ".csv"), 'w')
+tf.write(ground_truth)
+tf.close()

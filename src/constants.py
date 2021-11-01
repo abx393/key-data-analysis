d1fc39a4e1f3d043d5e45d8f4a9347997f64fe0a
abx393 <abandari99@gmail.com>
2021-11-01 14:33:44 -0700

    refactor global constants to separate file

---
+++ src/constants.py
@@ -0,0 +1,29 @@
+"""
+Class Constants
+"""
+
+"""
+Associating each key with a finger
+Hand encoding:
+R = Right
+L = Left
+
+Finger encoding:
+T = Thumb
+I = Index
+M = Middle
+R = Ring 
+P = Pinky
+"""
+
+KEY_MAP = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", \
+           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", \
+            "space"]
+
+FINGERS = ["RT", "RI", "RM", "RR", "RP", "LT", "LI", "LM", "LR", "LP"]
+
+FINGER_MAP = ["LP", "LI", "LM", "LM", "LM", "LI", "LI", "RI", "RM", "RI", \
+              "RM", "RR", "RI", "RI", "RR", "RP", "LP", "LI", "LR", "LI", \
+              "RI", "LI", "LR", "LR", "RI", "LP", "RT"]
+
+# Side note: I tend to use RT for 'space' but same may use LT and RT interchangeably

44358bb6bdcc7cac0036b89a74adc1ca9b439293
abx393 <abandari99@gmail.com>
2021-11-02 22:56:34 -0700

    add more global constants

--- src/constants.py
+++ src/constants.py
@@ -18,12 +18,15 @@ P = Pinky
 
 KEY_MAP = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", \
            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", \
-            "space"]
+            "space", "shift", "right shift", "backspace", "'", "-", \
+           "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "&", "%", "+", "\\"]
 
 FINGERS = ["RT", "RI", "RM", "RR", "RP", "LT", "LI", "LM", "LR", "LP"]
 
 FINGER_MAP = ["LP", "LI", "LM", "LM", "LM", "LI", "LI", "RI", "RM", "RI", \
               "RM", "RR", "RI", "RI", "RR", "RP", "LP", "LI", "LR", "LI", \
-              "RI", "LI", "LR", "LR", "RI", "LP", "RT"]
+              "RI", "LI", "LR", "LR", "RI", "LP", "RT", "LP", "RP", "RP", \
+              "RP", "RR", "LP", "LR", "LM", "LI", "LI", "RI", "RI", "RM", \
+              "RR", "RR", ".", "RI", "LI", "RP", "RP"]
 
-# Side note: I tend to use RT for 'space' but same may use LT and RT interchangeably
+# Side note: I tend to use RT for 'space' but some may use LT and RT interchangeably

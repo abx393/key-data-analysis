0353cddf175cc9db57f91aac76a54045e3a3c036
abx393 <abandari99@gmail.com>
2021-11-03 23:34:34 -0700

    limit keys to alphabet+spacebar to reduce clutter in viz

--- src/time_series_analysis.py
+++ src/time_series_analysis.py
@@ -12,7 +12,7 @@ def handle(func, arr):
     try:
         return func(arr)
     except:
-        return np.nan
+        return -1
 
 OUTLIER_CUTOFF = 0.40
 
@@ -22,13 +22,12 @@ KEYBOARD = "HP_Spectre"
 DIR_OUT = "../features/time_series"
 
 # either 'mean' or 'median'
-CENTER = "mean"
+CENTER = "median"
+
+LIMITED_KEYS = 27
 
 nn_features = open(os.path.join(DIR_OUT, "key_pairs.csv"), 'w')
 nn_features.write("prev_key,curr_key,time_delay\n")
-
-lstm_features = open(os.path.join(DIR_OUT, "key_forecasting.csv"), 'w')
-lstm_features.write("prev_")
 
 time_delay_by_key_pair = [[[] for i in range(len(constants.KEY_MAP))] for j in range(len(constants.KEY_MAP))]
 time_delay_by_finger_pair = [[[] for i in range(len(constants.FINGERS))] for j in range(len(constants.FINGERS))]
@@ -83,48 +82,46 @@ nn_features.close()
 # Plot average time delay for all key pairs
 fig, ax = plt.subplots()
 if CENTER == "mean":
-    im = ax.imshow([[handle(mean, arr) for arr in arr2] for arr2 in time_delay_by_key_pair])
+    im = ax.imshow([[handle(mean, time_delay_by_key_pair[i][j]) for j in range(LIMITED_KEYS)] for i in range(LIMITED_KEYS)])
 elif CENTER == "median":
-    im = ax.imshow([[handle(median, arr) for arr in arr2] for arr2 in time_delay_by_key_pair])
+    im = ax.imshow([[handle(median, time_delay_by_key_pair[i][j]) for j in range(LIMITED_KEYS)] for i in range(LIMITED_KEYS)])
 
-for i in range(len(time_delay_by_key_pair)):
-    for j in range(len(time_delay_by_key_pair[i])):
-        try:
+for i in range(LIMITED_KEYS):
+    for j in range(LIMITED_KEYS):
+        if len(time_delay_by_key_pair[i][j]) > 0:
             if CENTER == "mean":
                 text = ax.text(j, i, round(handle(mean, time_delay_by_key_pair[i][j]), 2), ha="center", va="center", color="w")
             elif CENTER == "median":
                 text = ax.text(j, i, round(handle(median, time_delay_by_key_pair[i][j]), 2), ha="center", va="center", color="w")
-        except:
-            text = ax.text(j, i, "NaN", ha="center", va="center", color="w")
 
         text.set_color('black')
         text.set_size(8)
 
 ax.set_title("Average Time Delay (s)")
-labels = [(constants.FINGER_MAP[i] + "    " + constants.KEY_MAP[i]) for i in range(len(constants.KEY_MAP))]
-ax.set_xticks(np.arange(len(constants.KEY_MAP)))
+labels = [(constants.FINGER_MAP[i] + "    " + constants.KEY_MAP[i]) for i in range(LIMITED_KEYS)]
+ax.set_xticks(np.arange(LIMITED_KEYS))
 ax.set_xticklabels(labels, rotation='vertical')
-ax.set_yticks(np.arange(len(constants.KEY_MAP)))
+ax.set_yticks(np.arange(LIMITED_KEYS))
 ax.set_yticklabels(labels)
 
-fig.tight_layout()
+#fig.tight_layout()
 plt.show()
 plt.close()
 
 # Plot number of data points for all key pairs
 fig, ax = plt.subplots()
 im = ax.imshow([[len(arr) for arr in arr2] for arr2 in time_delay_by_key_pair], cmap='hot', interpolation='nearest')
-for i in range(len(time_delay_by_key_pair)):
-    for j in range(len(time_delay_by_key_pair[i])):
+for i in range(LIMITED_KEYS):
+    for j in range(LIMITED_KEYS):
         text = ax.text(j, i, len(time_delay_by_key_pair[i][j]), ha="center", va="center", color="w")
         text.set_color('black')
         text.set_size(8)
 
 labels = [(constants.FINGER_MAP[i] + "    " + constants.KEY_MAP[i]) for i in range(len(constants.KEY_MAP))]
 ax.set_title("Number of Data Points for Each Key Pair")
-ax.set_xticks(np.arange(len(constants.KEY_MAP)))
+ax.set_xticks(np.arange(LIMITED_KEYS))
 ax.set_xticklabels(labels, rotation='vertical')
-ax.set_yticks(np.arange(len(constants.KEY_MAP)))
+ax.set_yticks(np.arange(LIMITED_KEYS))
 ax.set_yticklabels(labels)
 
 fig.tight_layout()

ce3208c4a8b9ff9207db882de1656a53e4cad5e4
abx393 <abandari99@gmail.com>
2022-01-19 11:28:57 -0800

    Plotting word-level time series aggregate data

--- src/time_series_word_analysis.py
+++ src/time_series_word_analysis.py
@@ -8,41 +8,93 @@ import numpy as np
 from json import JSONDecoder
 import matplotlib.pyplot as plt
 
-DIR_IN = "web_raw_data"
-SUBDIR_IN = "time_series"
-DIR_OUT = "plots"
-SUBDIR_OUT = "time_series"
+# words = ["gamma", "pedal", "brake", "climb", "walks", "hello", "world", "smile", "zebra", "space", "rover", "gears", "wheel", "stain"]
+words = ["the", "why", "ask", "mail", "data", "soon", "wall", "hold", "four", "gold", "thyme", "under",
+           "gamma", "pedal", "brake", "climb", "walks", "hello", "world", "smile", "zebra", "space", "rover", "gears", "wheel", "stain",
+           "static", "corral", "source", "groggy", "eleven", "quotes", "slight", "reward", "joseph"]
+y = {}
+for i, w in enumerate(words):
+    y[w] = []
 
-for f in os.listdir(os.path.join(DIR_IN, SUBDIR_IN)):
+DIR_IN = "native_raw_data_time_series_words"
+KEYBOARD = "HP_Spectre"
+#DIR_OUT = "plots"
+#SUBDIR_OUT = "time_series"
+
+for f in os.listdir(os.path.join(DIR_IN, KEYBOARD)):
     (basename, extension) = f.split(".")
 
+    # ignore audio data for now, only look at ground truth
+    if extension == "wav":
+        continue
+
     if extension == "json":
-        labels_file = open(os.path.join(DIR_IN, SUBDIR_IN, f))
+        labels_file = open(os.path.join(DIR_IN, KEYBOARD, f))
         labels = JSONDecoder().decode(labels_file.read())
 
-        t_prev = 0
+        prev_time = 0
         ts = np.asarray(list(labels.keys()), dtype=np.int)
         keys = np.array(list(labels.values()))
+    elif extension == "csv":
+        fr = open(os.path.join(DIR_IN, KEYBOARD, f))
+        i = 0
+        keys = []
+        ts = []
+        for line in fr:
+            if i > 0:
+                try:
+                    [curr_key, curr_time] = line.split(',')
+                    keys.append(curr_key)
+                    curr_time = float(curr_time)
+                    ts.append(1000 * curr_time)
+                except:
+                    print("i=", i)
+            i += 1
 
-        # Plot the 1st difference of the list of timestamps
-        n = 1
-        x = np.arange(len(ts) - n)
-        y = np.diff(ts, n=n)
+    word = "".join(keys)
+    if word in y:
+        # Compute the 1st difference of the list of timestamps
+        time_delays = np.diff(ts, n=1).tolist()
 
-        plt.plot(x, y)
-        plt.scatter(x, y)
+        # right-pad 'time_delays' with zeroes to length 5
+        while len(time_delays) < 5:
+            time_delays.append(0)
 
-        # Annotate the plot with which keys were pressed
-        ax = plt.gca()
-        for i in range(len(keys) - n):
-            ax.annotate(keys[i] + " -> " + keys[i+1], (i - 0.2, y[i] - 30))
+        y[word].append(time_delays)
 
-        word = ""
-        for key in keys:
-            word += key
+plt.title("Time delays by word")
+n = 1
+x = np.arange(len(ts) - n)
+cnt = 0
+for w in y:
+    cnt += 1
 
-        plt.title(word)
-        plt.ylim([0, 300])
-        plt.ylabel("Time interval (ms)")
-        plt.savefig(os.path.join(DIR_OUT, SUBDIR_OUT, word + ".jpg"))
+    time_delays = np.array(y[w])
+    assert type(time_delays) == np.ndarray
+    min_time_delays = np.min(time_delays, axis=0)
+    max_time_delays = np.max(time_delays, axis=0)
+    mean_time_delays = np.mean(time_delays, axis=0)
+    std_dev_time_delays = np.std(time_delays, axis=0)
+    #range_time_delays = np.ptp(time_delays, axis=0)
+    error_bars = [[mean_time_delays[i] - min_time_delays[i] for i in range(np.shape(time_delays)[1])],
+                  [max_time_delays[i] - mean_time_delays[i] for i in range(np.shape(time_delays)[1])]]
+
+    plt.errorbar(x, mean_time_delays, yerr=error_bars, label=w)
+    #plt.errorbar(x, mean_time_delays, label=w)
+    plt.scatter(x, mean_time_delays)
+
+    # Annotate the plot with which keys were pressed
+    ax = plt.gca()
+    for i in range(len(w) - n):
+        ax.annotate(w[i] + " -> " + w[i+1], (i - 0.25, mean_time_delays[i] - 5))
+        #ax.annotate(w, (i - 0.15, mean_time_delays[i] - 10))
+
+    plt.ylim([0, 300])
+    plt.xlim([-1, 5])
+    plt.ylabel("Time interval (ms)")
+    plt.legend()
+
+    if cnt % 3 == 0:
         plt.show()
+
+    # plt.savefig(os.path.join(DIR_OUT, SUBDIR_OUT, word + ".jpg"))

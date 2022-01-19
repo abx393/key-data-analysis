018dc7b7c54d0e4293e7532fbd1bccd59044b6b0
abx393 <abandari99@gmail.com>
2022-01-19 13:01:31 -0800

    dump trained model into output bin file

--- src/fft_model.py
+++ src/fft_model.py
@@ -15,6 +15,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
 
+from joblib import dump, load
+
 dir_in = "features"
 dir_out = "results"
 keyboard_type = "HP_Spectre"
@@ -36,18 +38,42 @@ f_distr.close()
 x = np.array(df.iloc[:, 1:])
 labels = np.array(df.iloc[:, 0])
 
-# y = np.array([1 if label == "backspace" else 0 for label in labels])
-# print(np.count_nonzero(y))
+# Space-or-not classifier
+y = np.array([1 if label == "space" else 0 for label in labels])
+# print("counts ", np.count_nonzero(y))
+# print("counts ", len(y) - np.count_nonzero(y))
+x_list = list(x)
+y_list = list(y)
+print(y_list)
+
+# Manually downsample larger class
+i = 0
+while i < len(x_list):
+    rand = np.random.rand() * 10
+    if y_list[i] == 0 and rand < 9:
+        x_list.pop(i)
+        y_list.pop(i)
+    else:
+        i += 1
 # print(np.shape(y))
+x = np.array(x_list)
+y = np.array(y_list)
+
+print("counts ", np.count_nonzero(y))
+print("counts ", len(y) - np.count_nonzero(y))
+
+"""
 if model == "SVM":
     y = labels
 else:
     lb = LabelBinarizer()
     y = lb.fit_transform(labels)
     classes = lb.classes_
+    print("classes ", classes)
     y = np.float64(y)
     print("x.shape ", x.shape)
     print("y.shape ", y.shape)
+"""
 
 # Split into train and test sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
@@ -71,6 +97,8 @@ elif model == "KMeans":
     clf = KMeans(n_clusters=3)
 
 clf.fit(x_train, y_train)
+dump(clf, 'models/FFT_Classification_space_{}.bin'.format(keyboard_type))
+dump(scaler, 'models/std_scaler_space_{}.bin'.format(keyboard_type), compress=True)
 
 #print(classes[np.argmax(y_pred, axis=1)])
 #print(lb.inverse_transform(y_pred))
@@ -82,9 +110,12 @@ if model == "SVM":
     acc = accuracy_score(y_test, y_pred)
     conf_matrix = confusion_matrix(y_test, y_pred)
 else:
-    y_pred = clf.predict_proba(x_test)
-    acc = accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
-    conf_matrix = confusion_matrix(classes[np.argmax(y_test, axis=1)], classes[np.argmax(y_pred, axis=1)], labels=classes)
+    #y_pred = clf.predict_proba(x_test)
+    #acc = accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
+    #conf_matrix = confusion_matrix(classes[np.argmax(y_test, axis=1)], classes[np.argmax(y_pred, axis=1)], labels=classes)
+    y_pred = clf.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
 
 #precision = precision_score(y_test, y_pred)
 #recall = recall_score(y_test, y_pred)
